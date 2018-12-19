72606197d360794cefd9511d926d0afe6057474a
Manikandan Shanmugam <e0284006@u.nus.edu>
2018-12-20 00:49:13 +0800

    Updates

---
+++ precache-manifest.31c5067637b09f064171eab2f5903dfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "587878060ea27aaeae3f",
+    "url": "/ReactMiniAspire/static/js/runtime~main.58787806.js"
+  },
+  {
+    "revision": "067ff5a75529892de828",
+    "url": "/ReactMiniAspire/static/js/main.067ff5a7.chunk.js"
+  },
+  {
+    "revision": "e792be22ae39753ee254",
+    "url": "/ReactMiniAspire/static/js/1.e792be22.chunk.js"
+  },
+  {
+    "revision": "067ff5a75529892de828",
+    "url": "/ReactMiniAspire/static/css/main.8b6b0aec.chunk.css"
+  },
+  {
+    "revision": "e792be22ae39753ee254",
+    "url": "/ReactMiniAspire/static/css/1.8551a15e.chunk.css"
+  },
+  {
+    "revision": "250ac4b31e1c04214caf57ed90705d3d",
+    "url": "/ReactMiniAspire/index.html"
+  }
+];
